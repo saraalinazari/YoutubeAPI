f66c850cc6fdae41427322382c7213d5a17d36cb
saraalinazari <sara.alinazari@gmail.com>
2018-04-15 22:55:09 -0700

    commit

--- assets/javascript/facebookAPI.js
+++ assets/javascript/facebookAPI.js
@@ -50,7 +50,7 @@
     //         })
     //     }
     // );
-   var queryURL="https://graph.facebook.com/v2.12/Sara Alinazari/posts?access_token=258507278047521|zcxXsMKuAnK4UYnpRSjiPy5YaHo&limit=3";
+   var queryURL="https://graph.facebook.com/v2.12/10207157941490688/posts?access_token=258507278047521|zcxXsMKuAnK4UYnpRSjiPy5YaHo&limit=3";
     $.ajax({
               url: queryURL,
               method: "GET"
