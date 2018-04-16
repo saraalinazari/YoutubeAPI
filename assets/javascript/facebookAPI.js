b4a2144793aee945a82d0fb2012adab021e49ed1
saraalinazari <sara.alinazari@gmail.com>
2018-04-15 23:06:33 -0700

    commit

--- assets/javascript/facebookAPI.js
+++ assets/javascript/facebookAPI.js
@@ -50,7 +50,8 @@
     //         })
     //     }
     // );
-   var queryURL="https://graph.facebook.com/v2.12/me/posts?fields=picture&access_token=258507278047521|zcxXsMKuAnK4UYnpRSjiPy5YaHo&limit=3";
+   //var queryURL="https://graph.facebook.com/v2.12/me/posts?fields=picture&access_token=258507278047521|zcxXsMKuAnK4UYnpRSjiPy5YaHo&limit=3";
+   var queryURL="https://graph.facebook.com/v2.12/me/posts?fields=cover&access_token=258507278047521|zcxXsMKuAnK4UYnpRSjiPy5YaHo&limit=3";
     $.ajax({
               url: queryURL,
               method: "GET"
